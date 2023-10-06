350a398a6d0067bb93526d3fb88aa4ea43682875
Spookypuss <114418331+Spookypuss@users.noreply.github.com>
2023-10-06 12:20:02 +0100

    Updated time zone selection functionality

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@
 // and format date to dddd Do MMMM
 
 // make function to update times every second
-function displayLocal() {
+/*function displayLocal() {
 let localTime = moment();
 let localTimeElement = document.querySelector("#local-time");
 localTimeElement.innerHTML = localTime.format("h:mm:ss [<small>]A[</small>]");
@@ -11,14 +11,9 @@ localDateElement.innerHTML = localTime.format("dddd Do MMMM YYYY");
 }
 
 displayLocal();
-setInterval(displayLocal, 1000); //updates function every second
+setInterval(displayLocal, 1000); //updates function every second*/
 
 function displayWorld() {
-let vancouverTime = moment().tz("America/Vancouver");
-let vancouverTimeElement = document.querySelector("#vancouver-time");
-vancouverTimeElement.innerHTML = vancouverTime.format("h:mm:ss [<small>]A[</small>]");
-let vancouverDateElement = document.querySelector("#vancouver-date");
-vancouverDateElement.innerHTML = vancouverTime.format("dddd Do MMMM YYYY");
 
 let saoPauloTime = moment().tz("America/Sao_Paulo");
 let saoPauloTimeElement = document.querySelector("#sao-paulo-time");
@@ -26,11 +21,11 @@ saoPauloTimeElement.innerHTML = saoPauloTime.format("h:mm:ss [<small>]A[</small>
 let saoPauloDateElement = document.querySelector("#sao-paulo-date");
 saoPauloDateElement.innerHTML = saoPauloTime.format("dddd Do MMMM YYYY"); //something is happening here
 
-let dublinTime = moment().tz("Europe/Dublin");
+/*let dublinTime = moment().tz("Europe/Dublin");
 let dublinTimeElement = document.querySelector("#dublin-time");
 dublinTimeElement.innerHTML = dublinTime.format("h:mm:ss [<small>]A[</small>]");
 let dublinDateElement = document.querySelector("#dublin-date");
-dublinDateElement.innerHTML = dublinTime.format("dddd Do MMMM YYYY");
+dublinDateElement.innerHTML = dublinTime.format("dddd Do MMMM YYYY");*/
 
 let karachiTime = moment().tz("Asia/Karachi");
 let karachiTimeElement = document.querySelector("#karachi-time");
@@ -38,11 +33,11 @@ karachiTimeElement.innerHTML = karachiTime.format("h:mm:ss [<small>]A[</small>]"
 let karachiDateElement = document.querySelector("#karachi-date");
 karachiDateElement.innerHTML = karachiTime.format("dddd Do MMMM YYYY");
 
-let tokyoTime = moment().tz("Asia/Tokyo");
+/*let tokyoTime = moment().tz("Asia/Tokyo");
 let tokyoTimeElement = document.querySelector("#tokyo-time");
 tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
 let tokyoDateElement = document.querySelector("#tokyo-date");
-tokyoDateElement.innerHTML = tokyoTime.format("dddd Do MMMM YYYY");
+tokyoDateElement.innerHTML = tokyoTime.format("dddd Do MMMM YYYY");*/
 
 let aucklandTime = moment().tz("Pacific/Auckland");
 let aucklandTimeElement = document.querySelector("#auckland-time");
@@ -57,6 +52,10 @@ setInterval(displayWorld, 1000);
 
 function updateCity(event) {
     let cityTimeZone = event.target.value;
+if (cityTimeZone === "current") {
+        cityTimeZone = moment.tz.guess();
+}
+
     let cityTime = moment().tz(cityTimeZone);
     let cityName = cityTimeZone.replace("_", " ").split("/")[1]; // replaces _ with space, then splits into array (by /) and uses second array item
 
@@ -64,7 +63,6 @@ function updateCity(event) {
 
     let citiesElement = document.querySelector("#cities");
     citiesElement.innerHTML = `
-                <br />
                 <div class="row">
                     <div class="col"> <!-- location and date first -->
                         <div class="row location">
