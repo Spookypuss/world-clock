f898a1549b8f2690f6ad301e71882dfc019559ea
Spookypuss <114418331+Spookypuss@users.noreply.github.com>
2023-10-06 11:38:43 +0100

    Added extra cities to page, added HTML injection on selection

--- src/app.js
+++ src/app.js
@@ -27,35 +27,58 @@ let saoPauloDateElement = document.querySelector("#sao-paulo-date");
 saoPauloDateElement.innerHTML = saoPauloTime.format("dddd Do MMMM YYYY"); //something is happening here
 
 let dublinTime = moment().tz("Europe/Dublin");
-let dublinTimeElement = document.querySelector("#dublin");
-dublinTimeElement.innerHTML = "hello?";
+let dublinTimeElement = document.querySelector("#dublin-time");
+dublinTimeElement.innerHTML = dublinTime.format("h:mm:ss [<small>]A[</small>]");
 let dublinDateElement = document.querySelector("#dublin-date");
 dublinDateElement.innerHTML = dublinTime.format("dddd Do MMMM YYYY");
+
+let karachiTime = moment().tz("Asia/Karachi");
+let karachiTimeElement = document.querySelector("#karachi-time");
+karachiTimeElement.innerHTML = karachiTime.format("h:mm:ss [<small>]A[</small>]");
+let karachiDateElement = document.querySelector("#karachi-date");
+karachiDateElement.innerHTML = karachiTime.format("dddd Do MMMM YYYY");
+
+let tokyoTime = moment().tz("Asia/Tokyo");
+let tokyoTimeElement = document.querySelector("#tokyo-time");
+tokyoTimeElement.innerHTML = tokyoTime.format("h:mm:ss [<small>]A[</small>]");
+let tokyoDateElement = document.querySelector("#tokyo-date");
+tokyoDateElement.innerHTML = tokyoTime.format("dddd Do MMMM YYYY");
+
+let aucklandTime = moment().tz("Pacific/Auckland");
+let aucklandTimeElement = document.querySelector("#auckland-time");
+aucklandTimeElement.innerHTML = aucklandTime.format("h:mm:ss [<small>]A[</small>]");
+let aucklandDateElement = document.querySelector("#auckland-date");
+aucklandDateElement.innerHTML = aucklandTime.format("dddd Do MMMM YYYY");
 
 }
 
 displayWorld();
 setInterval(displayWorld, 1000);
 
+function updateCity(event) {
+    let cityTimeZone = event.target.value;
+    let cityTime = moment().tz(cityTimeZone);
+    let cityName = cityTimeZone.replace("_", " ").split("/")[1]; // replaces _ with space, then splits into array (by /) and uses second array item
 
+    console.log(cityTime.format("h:mm:ss A"));
 
-let karachiTime = moment().tz("Asia/Karachi").format("hh:mm:ss A");
-let karachiDate = moment().tz("Asia/Karachi").format("dddd Do MMMM YYYY");
-let karachiTimeElement = document.querySelector("#karachi");
-karachiTimeElement.innerHTML = karachiTime;
-let karachiDateElement = document.querySelector("#karachi-date");
-karachiDateElement.innerHTML = karachiDate;
+    let citiesElement = document.querySelector("#cities");
+    citiesElement.innerHTML = `
+                <br />
+                <div class="row">
+                    <div class="col"> <!-- location and date first -->
+                        <div class="row location">
+                        ${cityName}
+                        </div>
+                        <div class="row date">${cityTime.format("dddd Do MMMM YYYY")}
+                    </div>
+                </div>
+                <div class="col"> <!-- time second -->
+                    <div class="clock">${cityTime.format("h:mm:ss [<small>]A[</small>]")}
+                    </div>
+                </div>
+                <hr />`
+}
 
-let tokyoTime = moment().tz("Asia/Tokyo").format("h:mm:ss A");
-let tokyoDate = moment().tz("Asia/Tokyo").format("dddd Do MMMM YYYY");
-let tokyoTimeElement = document.querySelector("#tokyo");
-tokyoTimeElement.innerHTML = tokyoTime;
-let tokyoDateElement = document.querySelector("#tokyo-date");
-tokyoDateElement.innerHTML = tokyoDate;
-
-let aucklandTime = moment().tz("Pacific/Auckland").format("hh:mm:ss A");
-let aucklandDate = moment().tz("Pacific/Auckland").format("dddd Do MMMM YYYY");
-let aucklandTimeElement = document.querySelector("#auckland");
-aucklandTimeElement.innerHTML = aucklandTime;
-let aucklandDateElement = document.querySelector("#auckland-date");
-aucklandDateElement.innerHTML = aucklandDate;
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
